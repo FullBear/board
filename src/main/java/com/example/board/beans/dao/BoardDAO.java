ebf49db1045f7c64949e5cd67840d82e51f1e56d
piru <pilus3363@gmail.com>
2021-10-11 16:39:11 +0900

    주석 작성

--- src/main/java/com/example/board/beans/dao/BoardDAO.java
+++ src/main/java/com/example/board/beans/dao/BoardDAO.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+//주석작성
 @Repository
 @RequiredArgsConstructor
 public class BoardDAO {
